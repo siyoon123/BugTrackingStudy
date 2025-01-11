eb8295fccba5d9452747a21eff36b532b6647f55
colby <colby@111percent.net>
2025-01-11 10:10:46 +0900

    feat: add examples

--- Assets/02 - Scripts/Managers/ExampleManager.cs
+++ Assets/02 - Scripts/Managers/ExampleManager.cs
@@ -1,13 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ExampleManager : MonoBehaviour
 {
     [SerializeField] private UIScale example_Popup_1;
+    [SerializeField] private UIScale example_Popup_2;
+    [SerializeField] private UIScale example_Popup_3;
+    [SerializeField] private Image example_Popup_3_Reddot;
+    
+    [SerializeField] private UIScale example_Popup_4;
 
     public void OnClickShowExamplePopup1()
     {
         example_Popup_1.Show();
+    }
+    
+    public void OnClickShowExamplePopup2()
+    {
+        example_Popup_2.Show();
+    }
+    
+    public void OnClickShowExamplePopup3()
+    {
+        example_Popup_3.Show();
+        example_Popup_3_Reddot.gameObject.SetActive(false);
+    }
+    
+    public void OnClickShowExamplePopup4()
+    {
+        example_Popup_4.Show();
     }
 }
